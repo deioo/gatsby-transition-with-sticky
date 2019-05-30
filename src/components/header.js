04c1bc9d737db4e90a419a551352fcaa18557243
Andrei Popa <andrei.deioo@gmail.com>
2019-05-30 09:38:25 +0100

    Make position:fixed work

--- src/components/header.js
+++ src/components/header.js
@@ -2,15 +2,21 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, fixed }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-      position: `sticky`,
-      position: `-webkit-sticky`,
-      top: 0,
-    }}
+    style={Object.assign(
+      {
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
+        top: 0,
+      },
+      fixed
+        ? { position: "fixed", top: 120 }
+        : {
+            position: `sticky`,
+            position: `-webkit-sticky`,
+          }
+    )}
   >
     <div
       style={{
@@ -37,10 +43,12 @@ const Header = ({ siteTitle }) => (
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  fixed: PropTypes.bool,
 }
 
 Header.defaultProps = {
   siteTitle: ``,
+  fixed: false,
 }
 
 export default Header
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,12 @@
 import React from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-
+import Header from "../components/header"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
+    <Header fixed siteTitle="position: fixed" />
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2. Scroll down.</p>
