0ddd6aef47b1cd6236d0281e2d8387b0fb838b59
Andrei Popa <andrei.deioo@gmail.com>
2019-05-30 08:57:00 +0100

    Initial commit - starter + anilink

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <AniLink to="/page-2/" paintDrip hex={"#fc0000"}>
+      Go to page 2
+    </AniLink>
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,18 @@
+import React from "react"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <AniLink to="/" paintDrip hex={"#fc0000"}>
+      Back to Homepage
+    </AniLink>
+  </Layout>
+)
+
+export default SecondPage
